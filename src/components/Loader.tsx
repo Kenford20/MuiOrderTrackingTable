2bebe72f7f959adbe3b0f76e082d0242f2f292b8
Kenford20 <kzhou0066@gmail.com>
2024-05-25 01:44:49 -0500

    fix: give loader backdrop higher z-index than modal

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,10 +2,7 @@ import { Backdrop, CircularProgress } from "@mui/material";
 
 export default function Loader({ open }: { open: boolean }) {
   return (
-    <Backdrop
-      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      open={open}
-    >
+    <Backdrop sx={{ color: "#fff", zIndex: "1500" }} open={open}>
       <CircularProgress color="inherit" />
     </Backdrop>
   );
